b6d4454bb2187b187760f4f5337c7ee65503db67
Jose <josedczarate@hotmail.com>
2018-09-06 21:11:48 -0500

    App Funcional sin validar

--- app/src/main/java/com/example/android/personasmovil/registrar.java
+++ app/src/main/java/com/example/android/personasmovil/registrar.java
@@ -2,12 +2,48 @@ package com.example.android.personasmovil;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class registrar extends Activity {
+
+    private EditText txtCedula, txtNombre, txtApellido;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar);
+
+        txtCedula = findViewById(R.id.txtCedula);
+        txtNombre = findViewById(R.id.txtNombre);
+        txtApellido= findViewById(R.id.txtApellido);
+
     }
+
+    public void guardar (View v){
+        String ced, nomb, apell;
+
+        ced = txtCedula.getText().toString();
+        nomb = txtNombre.getText().toString();
+        apell = txtApellido.getText().toString();
+
+        Persona p = new Persona(ced,nomb,apell);
+        p.guardar();
+        Toast.makeText(this, getResources().getString(R.string.mensaje),Toast.LENGTH_SHORT).show();
+        borrar();
+    }
+
+    public void limpiar(View v){
+        borrar();
+    }
+
+    private void borrar(){
+        txtCedula.setText("");
+        txtNombre.setText("");
+        txtApellido.setText("");
+        txtCedula.requestFocus();
+
+    }
+
 }
