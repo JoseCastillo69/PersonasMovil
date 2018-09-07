9a9f3bf42f313d92656307eeac632f3760f156fa
Jose <josedczarate@hotmail.com>
2018-09-06 19:47:57 -0500

    Interfaz Terminada

---
+++ app/src/main/java/com/example/android/personasmovil/listar.java
@@ -0,0 +1,13 @@
+package com.example.android.personasmovil;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class listar extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar);
+    }
+}
