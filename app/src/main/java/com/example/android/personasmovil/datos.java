c6bbd18e0210f5eb0e23d0f0ad18285d9adabd56
Jose <josedczarate@hotmail.com>
2018-09-06 20:38:13 -0500

    Conexion Ventanas Exitosa

---
+++ app/src/main/java/com/example/android/personasmovil/datos.java
@@ -0,0 +1,19 @@
+package com.example.android.personasmovil;
+
+import java.util.ArrayList;
+
+public class datos {
+
+    private static ArrayList<Persona> personas = new ArrayList<>();
+
+    public static void guardar (Persona p){
+        personas.add(p);
+    }
+
+    public static ArrayList<Persona> obtener(){
+        return personas;
+    }
+
+
+
+}
